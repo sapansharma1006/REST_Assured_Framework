3ea22d56d7125ca8b8ff69159e36894596e81a6a
sapansharma1006 <sapan.shrma@gmail.com>
2020-04-06 17:48:46 +0530

    changes for reporting

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/java/Features",
+plugin = "json:target/jsonReports/cucumber-report.json",
 glue={"StepDefinition"},tags = {"@All"})
 public class TestRunner {
 }
